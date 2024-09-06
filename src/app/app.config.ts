1ee3291386319ee547283a98343b384673eb0d7c
Rohitchaudhary2 <rohitbhambota1@gmail.com>
2024-09-06 19:57:35 +0530

    Routing

---
+++ src/app/app.config.ts
@@ -0,0 +1,9 @@
+import { provideRouter, withComponentInputBinding, withRouterConfig } from "@angular/router";
+import { routes } from "./app.routes";
+
+export const appConfig = {
+    providers: [provideRouter(routes, withComponentInputBinding(), 
+    withRouterConfig({
+        paramsInheritanceStrategy: 'always'
+    }))]
+}
