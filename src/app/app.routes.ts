a3d910193dbcdc7fe1a9e508322936b5db60ab35
Rohitchaudhary2 <rohitbhambota1@gmail.com>
2024-09-07 11:03:06 +0530

    Lazy Loading using Routed based approach

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from "@angular/router";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
-import { userRoutes } from "./users/user.route";
 
 export const routes: Routes = [
     {
@@ -12,7 +11,7 @@ export const routes: Routes = [
     {
         path: 'users/:userId',
         component: UserTasksComponent,
-        children: userRoutes
+        loadChildren: () => import('./users/user.route').then(mod => mod.userRoutes)
     },
     {
         path: '**',
--- src/app/users/user.route.ts
+++ src/app/users/user.route.ts
@@ -1,19 +1,28 @@
 import { Routes } from "@angular/router";
-import { NewTaskComponent } from "../tasks/new-task/new-task.component";
+import { canLeaveEditPage, NewTaskComponent } from "../tasks/new-task/new-task.component";
 import { TasksComponent } from "../tasks/tasks.component";
+import { TasksService } from "../tasks/tasks.service";
 
 export const userRoutes: Routes = [
     {
         path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix'
-    },
-    {
-        path: 'tasks',
-        component: TasksComponent
-    },
-    {
-        path: 'tasks/new',
-        component: NewTaskComponent
+        providers: [TasksService],
+        children:[
+            {
+                path: '',
+                redirectTo: 'tasks',
+                pathMatch: 'prefix'
+            },
+            {
+                path: 'tasks',
+                component: TasksComponent
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent,
+                canDeactivate: [canLeaveEditPage]
+            }
+        ]
     }
+    
 ]
